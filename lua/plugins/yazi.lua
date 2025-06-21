f34b9e56c24299d867c4e49d8be20432a9ae17df
chenzx <lyxx2413@outlook.com>
2025-06-22 05:13:10 +0800

    change yazi keymap

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -10,20 +10,20 @@ return {
     },
     keys = {
       -- 👇 in this section, choose your own keymappings!
+      -- {
+      --   "<leader>-",
+      --   mode = { "n", "v" },
+      --   "<cmd>Yazi<cr>",
+      --   desc = "Open yazi at the current file",
+      -- },
+      -- {
+      --   -- Open in the current working directory
+      --   "<leader>E",
+      --   "<cmd>Yazi cwd<cr>",
+      --   desc = "Open the file manager in nvim's working directory",
+      -- },
       {
-        "<leader>-",
-        mode = { "n", "v" },
-        "<cmd>Yazi<cr>",
-        desc = "Open yazi at the current file",
-      },
-      {
-        -- Open in the current working directory
         "<leader>E",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open the file manager in nvim's working directory",
-      },
-      {
-        "<c-up>",
         "<cmd>Yazi toggle<cr>",
         desc = "Resume the last yazi session",
       },
